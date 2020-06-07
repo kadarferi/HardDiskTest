baa8be366f164ee33917514a64592c733aa82b44
kadarferi <kadarferi@gmail.com>
2020-06-07 13:07:30 +0200

    First commit - ST500 test done

---
+++ s032R.bat
@@ -0,0 +1,2 @@
+start cmd /k sqlio -kR -t30 -s30 -f64 -b32 -i1024 -BN testfileR.dat
+rem start cmd /k sqlio -kW -t30 -s30 -f64 -b32 -i1024 -BN testfileW.dat
---
+++ s064WR.bat
@@ -0,0 +1,2 @@
+start cmd /k sqlio -kR -t30 -s30 -f64 -b64 -i1024 -BN testfileR.dat
+start cmd /k sqlio -kW -t30 -s30 -f64 -b64 -i1024 -BN testfileW.dat
